c5ab038eeed6574c745f3ff20f32ba2ae36a1daf
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-02-02 17:33:59 +0100

    get user input

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"hangman/hangman"
+	"os"
 )
 
 func main() {
 	g := hangman.New(8, "Golang")
 	fmt.Println(g)
+
+	l, err := hangman.ReadGuess()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(l)
 }
